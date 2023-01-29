03f4d6d1c16078231b742adf1c12a45bf294fd12
whatsweb3org <whatsweb3org@gmail.com>
2023-01-29 14:01:56 +0800

    fix: try it path

--- src/components/TryIt.js
+++ src/components/TryIt.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import {useLocation} from '@docusaurus/router';
 
-
 const TryIt = ( { fileName } ) => {
   const location = useLocation();
+
+  var splittedPath = location.pathname.split('/');
+  var baseName = splittedPath.pop();
+  splittedPath.push("sample-code", baseName, fileName);
+  var pathToCode = splittedPath.join('/');
   return (
     <Link
         className={'button button--lg button--primary'}
-        href={'https://remix.ethereum.org/whatsweb3org/website/blob/main' + location.pathname + '/' + fileName}
-        // href={'https://remix.ethereum.org/whatsweb3org/website/blob/main/docs/solidity-basic/sample-code/all-code/AddrMemberVariable.sol'}
+        href={'https://remix.ethereum.org/whatsweb3org/website/blob/main' + pathToCode}
     >
       { '试一试 >> ' }
     </Link>
